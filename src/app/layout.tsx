c8a27e7f4fdd320a2126c943c5a91ec294796572
Giovannidjuric <giovanni.djuric@hotmail.com>
2025-01-05 21:53:19 +0100

    started project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="sm:flex justify-between">
+          <div className="">LeftBar</div>
+          <div className="">{children}</div>
+          <div className="">RightBar</div>
+        </div>
+      </body>
     </html>
   );
 }
